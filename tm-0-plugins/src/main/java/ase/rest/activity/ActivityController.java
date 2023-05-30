0798eb9784fe308baf460ee80e43352204d86dd3
bloup-dv <tom.naujox@gmail.com>
2023-05-30 06:07:45 +0200

    added rest controllers

---
+++ tm-0-plugins/src/main/java/ase/rest/activity/ActivityController.java
@@ -0,0 +1,31 @@
+package ase.rest.activity;
+
+import ase.activity.CreateActivitiesService;
+import ase.activity.ActivityDTO;
+import ase.activity.ActivityDTOToActivityMapper;
+import ase.activity.ActivityToActivityDTOMapper;
+import ase.category.CategoryDTOToCategoryMapper;
+import ase.category.CategoryToCategoryDTOMapper;
+import ase.types.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/activity")
+public class ActivityController {
+    private final ActivityDTOToActivityMapper activityDTOToActivityMapper;
+    private final ActivityToActivityDTOMapper activityToActivityDTOMapper;
+    private final CreateActivitiesService createActivitiesService;
+    @Autowired
+    public ActivityController(ActivityDTOToActivityMapper activityDTOToActivityMapper,
+                              ActivityToActivityDTOMapper activityToActivityDTOMapper,
+                              CreateActivitiesService createActivitiesService) {
+        this.activityDTOToActivityMapper = activityDTOToActivityMapper;
+        this.activityToActivityDTOMapper = activityToActivityDTOMapper;
+        this.createActivitiesService = createActivitiesService;
+    }
+    @PostMapping(value = "/create_activity")
+    public void createActivity(@RequestBody ActivityDTO activityDTO) throws IllegalArgumentException
+    {
+        this.createActivitiesService.createActivity(this.activityDTOToActivityMapper.apply(activityDTO));
+    }
+}
---
+++ tm-0-plugins/src/main/java/ase/rest/session/SessionController.java
@@ -0,0 +1,9 @@
+package ase.rest.session;
+
+public class SessionController {
+    //service, DTOmapper
+
+    //constructor
+
+    //post(/"startSession") @Body SessionDTO {Sessionservice.create(ActivityDTO.apply(activity))}
+}

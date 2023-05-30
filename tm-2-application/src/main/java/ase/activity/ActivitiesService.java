25defe66e756dc3fcdd75dfcbcba147ee565e40d
bloup-dv <tom.naujox@gmail.com>
2023-05-30 22:09:32 +0200

    REFACTORING ActivitiesService

--- tm-2-application/src/main/java/ase/activity/ActivitiesService.java
+++ tm-2-application/src/main/java/ase/activity/ActivitiesService.java
@@ -26,71 +26,41 @@ public class ActivitiesService {
     }
 
     public void createActivity(Activity activity) throws IllegalArgumentException {
-        if (activity == null || activity.getCategory() == null || activity.getEstimation() == null) {
-            throw new IllegalArgumentException("the Action is incomplete");
-        }
-        if (activity.getDueDate() == null) {
-            throw new IllegalArgumentException("please assign a valid due date to the activity");
-        }
-        LocalDateTime currentTime = LocalDateTime.now();
-        if (currentTime.isAfter(activity.getDueDate())) {
-            throw new IllegalArgumentException("the due date must be in the future");
-        }
-        if (currentTime.plus(activity.getEstimation().getEstimation(), ChronoUnit.MINUTES)
-                .isAfter(activity.getDueDate())) {
-            throw new IllegalArgumentException(
-                    "the Estimated duration may not take longer than the total time until the due date");
-        }
-        if (activity.getName() == null) {
-            throw new IllegalArgumentException("the Activity must have a name");
-        }
+        isActivityValid(activity);
+        checkActivityDates(activity);
+        checkActivityName(activity);
         activityRepository.save(new Activity(activity.getName(), activity.getDueDate(), activity.getEstimation(),
                 activity.getCategory()));
     }
 
     public void editActivity(Long oldActivityId, Activity newActivity) throws IllegalArgumentException {
-        if (!this.activityRepository.existsById(oldActivityId)) {
-            throw new IllegalArgumentException("this Activity does not exist");
-        }
-        if (newActivity == null || newActivity.getCategory() == null || newActivity.getEstimation() == null) {
-            throw new IllegalArgumentException("the Action is incomplete");
-        }
-        if (!this.categoryRepository.existsById(newActivity.getCategory().getId())) {
-            throw new IllegalArgumentException("chosen Category does not exist");
-        }
-        if (newActivity.getDueDate() == null) {
-            throw new IllegalArgumentException("please assign a valid due date to the activity");
-        }
-        LocalDateTime currentTime = LocalDateTime.now();
-        if (currentTime.isAfter(newActivity.getDueDate())) {
-            throw new IllegalArgumentException("the due date must be in the future");
-        }
-        if (currentTime.plus(newActivity.getEstimation().getEstimation(), ChronoUnit.MINUTES)
-                .isAfter(newActivity.getDueDate())) {
-            throw new IllegalArgumentException(
-                    "the Estimated duration may not take longer than the total time until the due date");
-        }
-        if (newActivity.getName() == null) {
-            throw new IllegalArgumentException("the Activity must have a name");
-        }
+        checkIfActivityExists(oldActivityId);
+        isActivityValid(newActivity);
+        isCategoryValid(newActivity.getCategory());
+        checkActivityDates(newActivity);
+        checkActivityName(newActivity);
         activityRepository.save(new Activity(oldActivityId, newActivity.getName(), newActivity.getDueDate(),
                 newActivity.getEstimation(), newActivity.getCategory(), newActivity.getStatus()));
     }
 
     public List<Activity> listByCategory(Category category) throws IllegalArgumentException {
+        isCategoryValid(category);
+        return this.activityRepository.findAllByCategory(category);
+
+    }
+
+    private void isCategoryValid(Category category) {
         if (category == null) {
             throw new IllegalArgumentException("Category is invalid.");
         }
         if (!this.categoryRepository.existsById(category.getId())) {
             throw new IllegalArgumentException("Category does not exist");
         }
-        return this.activityRepository.findAllByCategory(category);
-
     }
 
     public List<Activity> listByStatus(Status status) throws IllegalArgumentException {
         if (status == null) {
-            throw new IllegalArgumentException("Category is invalid.");
+            throw new IllegalArgumentException("Status is invalid.");
         }
         List<Activity> activityList = this.activityRepository.findAllByStatus(status);
         Collections.sort(activityList, Comparator.comparing(Activity::getDueDate));
@@ -102,5 +72,38 @@ public class ActivitiesService {
             throw new IllegalArgumentException("the Activity does not exist");
         }
         this.activityRepository.delete(activity);
+    }
+
+    private void isActivityValid(Activity activity) {
+        if (activity == null || activity.getCategory() == null || activity.getEstimation() == null) {
+            throw new IllegalArgumentException("the activity is incomplete");
+        }
+        if (activity.getDueDate() == null) {
+            throw new IllegalArgumentException("please assign a valid due date to the activity");
+        }
+    }
+
+    private void checkActivityName(Activity activity) {
+        if (activity.getName() == null) {
+            throw new IllegalArgumentException("the Activity must have a name");
+        }
+    }
+
+    private void checkActivityDates(Activity activity) {
+        LocalDateTime currentTime = LocalDateTime.now();
+        if (currentTime.isAfter(activity.getDueDate())) {
+            throw new IllegalArgumentException("the due date must be in the future");
+        }
+        if (currentTime.plus(activity.getEstimation().getEstimation(), ChronoUnit.MINUTES)
+                .isAfter(activity.getDueDate())) {
+            throw new IllegalArgumentException(
+                    "the Estimated duration may not take longer than the total time until the due date");
+        }
+    }
+
+    private void checkIfActivityExists(Long oldActivityId) {
+        if (!this.activityRepository.existsById(oldActivityId)) {
+            throw new IllegalArgumentException("the activity does not exist");
+        }
     }
 }
